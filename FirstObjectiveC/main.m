e606292fbfc493d81b4d5ba176a5ba4695348580
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-07-09 15:14:06 +0900

    property 옵션을 이용한 getter setter 만들기

--- FirstObjectiveC/main.m
+++ FirstObjectiveC/main.m
@@ -12,44 +12,50 @@
 //객체 설정
 @interface Vehicle : NSObject{
         //1.member variable -  맴버 변수 선언
-        int wheels;
-        int seats;
+//        int wheels;
+//        int seats;
 }
 
 //2.member method - 맴버 메서드 선언
--(void)setWheels:(int)w;
--(void)setSeats:(int)s;
-//getter
--(int)wheels;
--(int)seats;
--(void)print;
 
+//getter setter 대신에 property 작성
+@property (getter=getWheels, setter=wheels:)int wheels;
+@property int seats;
+
+//-(void)setWheels:(int)w;
+//-(void)setSeats:(int)s;
+////getter
+//-(int)wheels;
+//-(int)seats;
+//-(void)print;
 @end
 
 
 //3.Vehicle 객체 상속해서 메서드 구현
 @implementation Vehicle
--(void)setWheels:(int)w{
-    wheels = w;
-}
+@synthesize wheels;
+@synthesize seats;
 
--(void)setSeats:(int)s{
-    seats = s;
-}
-
-
--(int)wheels{
-    return wheels;
-}
-
--(int)seats{
-    return seats;
-}
+//-(void)setWheels:(int)w{
+//    wheels = w;
+//}
+//
+//-(void)setSeats:(int)s{
+//    seats = s;
+//}
+//
+////getter 메서드 구현
+//-(int)wheels{
+//    return wheels;
+//}
+//
+//-(int)seats{
+//    return seats;
+//}
 
 -(void)print{
     NSLog(@"wheels : %i, seats : %i" , wheels, seats);
 }
-
 @end
 
 int main(int argc, const char * argv[]) {
@@ -58,13 +64,13 @@ int main(int argc, const char * argv[]) {
         Vehicle *hello = [Vehicle new]; //create instance object
         
         //5. 값 할당 및 출력
-        [hello setWheels: 4];
+        [hello wheels: 4];
         [hello setSeats: 2];
         
         //삭제하고
         //[hello print];
         //getter 호출
-        NSLog(@"wheels : %i, seats : %i", [hello wheels], [hello seats]);
+        NSLog(@"wheels : %i, seats : %i", [hello getWheels], [hello seats]);
     }
     return 0;
 }
